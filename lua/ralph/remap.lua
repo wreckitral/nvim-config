26fb198480c84da545742d064a3723bcdf96a07d
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-04-30 20:45:27 +0700

    docs: add docs for the remap

--- lua/ralph/remap.lua
+++ lua/ralph/remap.lua
@@ -4,15 +4,22 @@ vim.g.mapleader = " "
 -- Exit to directory
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- in v mode J for moving block of code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+-- in n mode make code under go inline
 vim.keymap.set("n", "J", "mzJ`z")
+
+-- in n mode jump down and up
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- in n mode jump to words that is just searched  
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- start vim with me plugin
 vim.keymap.set("n", "<leader>vwm", function()
     require("vim-with-me").StartVimWithMe()
 end)
@@ -20,16 +27,19 @@ vim.keymap.set("n", "<leader>svwm", function()
     require("vim-with-me").StopVimWithMe()
 end)
 
--- greatest remap ever
+-- pasting without having to copy again
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- delete without copy to buffer
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+
 -- next greatest remap ever : asbjornHaland
+-- in v mode copy to clipboard
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
 -- This is going to get me cancelled
+-- in insert mode pressing i m
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
@@ -44,10 +54,11 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+-- in normal mode go to packer file
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim/lua/ralph/packer.lua<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
-
+-- end
