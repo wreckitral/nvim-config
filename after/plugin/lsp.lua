0d69d392fa4a6d5181e16be3998237cec363cfb5
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-26 00:43:04 +0700

    fix: configure so jdtl only runs if its java file

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -52,63 +52,66 @@ cmp.setup({
 })
 
 -- Configure nvim-jdtls with Lombok support
-local jdtls = require('jdtls')
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'java',
+  callback = function()
+    local jdtls = require('jdtls')
 
-local root_markers = {'pom.xml', '.git'}
-local root_dir = require('jdtls.setup').find_root(root_markers)
+    local root_markers = {'pom.xml', '.git'}
+    local root_dir = require('jdtls.setup').find_root(root_markers)
 
-if root_dir then
-  local home = os.getenv('HOME')
-  local lombok_path = home .. '/.m2/repository/org/projectlombok/lombok/1.18.32/lombok-1.18.32.jar'
-  local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
+    if root_dir then
+      local home = os.getenv('HOME')
+      local lombok_path = home .. '/.m2/repository/org/projectlombok/lombok/1.18.24/lombok-1.18.24.jar'
+      local workspace_folder = home .. "/.local/share/eclipse/" .. vim.fn.fnamemodify(root_dir, ":p:h:t")
 
-  local config = {
-    cmd = {
-      'java',
-      '-Declipse.application=org.eclipse.jdt.ls.core.id1',
-      '-Dosgi.bundles.defaultStartLevel=4',
-      '-Declipse.product=org.eclipse.jdt.ls.core.product',
-      '-Dlog.protocol=true',
-      '-Dlog.level=ALL',
-      '-Xms1g',
-      '--add-modules=ALL-SYSTEM',
-      '--add-opens', 'java.base/java.util=ALL-UNNAMED',
-      '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
-      '-javaagent:' .. lombok_path,
-      '-jar', vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
-      '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
-      '-data', workspace_folder
-    },
-    root_dir = root_dir,
-    settings = {
-      java = {
-        eclipse = {
-          downloadSources = true,
+      local config = {
+        cmd = {
+          'java',
+          '-Declipse.application=org.eclipse.jdt.ls.core.id1',
+          '-Dosgi.bundles.defaultStartLevel=4',
+          '-Declipse.product=org.eclipse.jdt.ls.core.product',
+          '-Dlog.protocol=true',
+          '-Dlog.level=ALL',
+          '-Xms1g',
+          '--add-modules=ALL-SYSTEM',
+          '--add-opens', 'java.base/java.util=ALL-UNNAMED',
+          '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
+          '-javaagent:' .. lombok_path,
+          '-jar', vim.fn.glob(home .. '/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
+          '-configuration', home .. '/.local/share/nvim/mason/packages/jdtls/config_linux',
+          '-data', workspace_folder
         },
-        configuration = {
-          updateBuildConfiguration = 'interactive',
+        root_dir = root_dir,
+        settings = {
+          java = {
+            eclipse = {
+              downloadSources = true,
+            },
+            configuration = {
+              updateBuildConfiguration = 'interactive',
+            },
+            maven = {
+              downloadSources = true,
+            },
+            implementationsCodeLens = {
+              enabled = true,
+            },
+            referencesCodeLens = {
+              enabled = true,
+            },
+            format = {
+              enabled = true,
+            },
+          },
         },
-        maven = {
-          downloadSources = true,
+        init_options = {
+          bundles = {},
         },
-        implementationsCodeLens = {
-          enabled = true,
-        },
-        referencesCodeLens = {
-          enabled = true,
-        },
-        format = {
-          enabled = true,
-        },
-      },
-    },
-    init_options = {
-      bundles = {},
-    },
-  }
+      }
 
-  -- Start or attach JDTLS
-  pcall(function()
-    jdtls.start_or_attach(config)
-  end)
-end
+      -- Start or attach JDTLS
+      jdtls.start_or_attach(config)
+    end
+  end,
+})
