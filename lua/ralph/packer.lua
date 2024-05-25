b687c3062fb161f4726f969a4efc1533e6a75b8d
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-25 09:15:53 +0700

    build: install depedency for java development

--- lua/ralph/packer.lua
+++ lua/ralph/packer.lua
@@ -76,6 +76,33 @@ return require('packer').startup(function(use)
             {'L3MON4D3/LuaSnip'},
         }
     }
+
+    -- Java support
+    use {
+        'nvim-java/nvim-java',
+        requires = {
+            'nvim-java/lua-async-await',
+            'nvim-java/nvim-java-refactor',
+            'nvim-java/nvim-java-core',
+            'nvim-java/nvim-java-test',
+            'nvim-java/nvim-java-dap',
+            'MunifTanjim/nui.nvim',
+            'neovim/nvim-lspconfig',
+            'mfussenegger/nvim-dap',
+            {
+                'williamboman/mason.nvim',
+                opts = {
+                    registries = {
+                        'github:nvim-java/mason-registry',
+                        'github:mason-org/mason-registry',
+                    },
+                },
+            },
+        },
+    }
+
+-- JDTLS plugin
+use 'mfussenegger/nvim-jdtls'
     -- Multicursors
     use {
         'mg979/vim-visual-multi',
