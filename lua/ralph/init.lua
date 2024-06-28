e36d3a8c03232be810e48c66271be9fb7a30e293
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-28 16:31:51 +0700

    feat: attach lsp

--- lua/ralph/init.lua
+++ lua/ralph/init.lua
@@ -29,11 +29,28 @@ autocmd('TextYankPost', {
     end,
 })
 
--- delete blank line at the end of a file
+-- delete blank line at the end of a line
 autocmd({"BufWritePre"}, {
     group = RalphGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
+})
+
+autocmd('LspAttach', {
+    group = RalphGroup,
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    end
 })
 
 -- file browser clean look
