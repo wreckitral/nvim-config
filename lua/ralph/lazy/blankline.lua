65a9b8a5ffbdaa9276afbb2581c2d11254627d9a
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-06-28 16:43:25 +0700

    feat: config blankline

---
+++ lua/ralph/lazy/blankline.lua
@@ -0,0 +1,9 @@
+return {
+    "lukas-reineke/indent-blankline.nvim",
+
+    config = function ()
+        require("ibl").setup {
+            exclude = { filetypes = {"dashboard"} }
+        }
+    end
+}
