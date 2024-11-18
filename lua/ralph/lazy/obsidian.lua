81a4b9b38bc16a52a9cd2ca473ebe6a8a3021865
wreckitral <defhanayasofhiea@gmail.com>
2024-11-18 10:35:03 +0700

    feat: tried to add image viewing feature on nvim

--- lua/ralph/lazy/obsidian.lua
+++ lua/ralph/lazy/obsidian.lua
@@ -1,21 +1,10 @@
 return {
     "epwalsh/obsidian.nvim",
-    version = "*",  -- recommended, use latest release instead of latest commit
+    version = "*",
     lazy = true,
     ft = "markdown",
-    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-    -- event = {
-    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-    --   -- refer to `:h file-pattern` for more examples
-    --   "BufReadPre path/to/my-vault/*.md",
-    --   "BufNewFile path/to/my-vault/*.md",
-    -- },
     dependencies = {
-        -- Required.
         "nvim-lua/plenary.nvim",
-
-        -- see below for full list of optional dependencies 👇
     },
     opts = {
         workspaces = {
@@ -24,19 +13,12 @@ return {
                 path = "~/exocortex",
             },
         },
-        -- see below for full list of options 👇
         templates = {
             folder = "Templates"
         },
-        -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-        -- URL it will be ignored but you can customize this behavior here.
-        ---@param url string
-        follow_url_func = function(url)
-            -- Open the URL in the default web browser.
-            vim.fn.jobstart({"xdg-open", url})
-            -- vim.cmd(':silent exec "!start ' .. url .. '"') -- Windows
-            vim.ui.open(url) -- need Neovim 0.10.0+
-        end,
-    },
 
+        follow_url_func = function(url)
+            vim.fn.jobstart({ "xdg-open", url })
+        end, -- Remove extra `vim.ui.open(url)` as `xdg-open` should handle opening the URL.
+    },
 }
