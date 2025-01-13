5eb36f88c615a0bc6a5e54024ef9a24acf742540
wreckitral <defhanayasofhiea@gmail.com>
2025-01-13 19:37:24 +0700

    feat: add comment to telescope

--- lua/ralph/lazy/telescope.lua
+++ lua/ralph/lazy/telescope.lua
@@ -11,19 +11,19 @@ return {
         require('telescope').setup({})
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- find file
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- find file from git
+        vim.keymap.set('n', '<leader>pws', function() -- find by word
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>pWs', function()
+        vim.keymap.set('n', '<leader>pWs', function() -- current word under cursor
             local word = vim.fn.expand("<cWORD>")
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
+        vim.keymap.set('n', '<leader>ps', function() -- find words by string manually
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {}) -- find help
     end
 }
