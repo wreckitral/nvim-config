06f10abdf91e5e18ffa34a223052e8e503800eab
wreckitral <defhanayasofhiea@gmail.com>
2025-08-22 10:53:02 +0700

    feat: create snippets

--- lua/ralph/lazy/lsp.lua
+++ lua/ralph/lazy/lsp.lua
@@ -16,6 +16,7 @@ return {
     config = function()
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
+        require("ralph.snippets.cuda")
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
