e632f6ac3bc939d26aec89b565cc2a3413c3a66b
defha <defhanayasofhiea@gmail.com>
2026-07-28 18:39:07 +0700

    feat: update nvim

--- lua/ralph/lazy/vimtex.lua
+++ lua/ralph/lazy/vimtex.lua
@@ -5,5 +5,6 @@ return {
     init = function()
         -- VimTeX configuration goes here, e.g.
         vim.g.vimtex_view_method = "zathura"
+        vim.g.vimtex_syntax_conceal_disable = 1
     end
 }
